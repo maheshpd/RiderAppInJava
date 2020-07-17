0601531defe806f46bebdfb60298382cc34538d2
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-17 09:14:55 +0530

    Rider App Register User

---
+++ app/src/main/java/com/createsapp/riderappjava/Common/Common.java
@@ -0,0 +1,8 @@
+package com.createsapp.riderappjava.Common;
+
+import com.createsapp.riderappjava.model.RiderModel;
+
+public class Common {
+    public static final String RIDER_INFO_REFERENCE = "Riders";
+    public static RiderModel currentRider;
+}
